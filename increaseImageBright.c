a8318b5e9cc55998ab05445767f00a3ae8bef595
blackaly <algeserone@yandex.com>
2022-11-08 12:10:02 +0200

    B&W Image

--- increaseImageBright.c
+++ increaseImageBright.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 
 	FILE* streamIn;
-	streamIn = fopen("./lena512.bmp", "r");
+	streamIn = fopen("./lena512.bmp", "rb");
 
 
 	unsigned char header[54];
@@ -16,22 +17,32 @@ int main(){
 	int height = *(int*)&header[22];
 	int bitDepth = *(int*)&header[28];
 
+	bool flag = (bitDepth <= 8) ? 1: 0;
 
-	if(bitDepth <= 8) fread(colorTable, sizeof(unsigned char), 1024, streamIn);
+
+	if(flag) fread(colorTable, sizeof(unsigned char), 1024, streamIn);
 
 	unsigned char buff[height * width];
 	fread(buff, sizeof(unsigned char), (height * width), streamIn);
+
+
+	for(int i = 0; i < height * width; ++i)
+		buff[i] += 25;
+
 
 	FILE *fo = fopen("./newimage.bmp", "wb");
 
 	fwrite(header, sizeof(unsigned char), 54, fo);
 
-	if(bitDepth <= 8) fwrite(colorTable, sizeof(unsigned char), 1024, fo);
+	if(flag) fwrite(colorTable, sizeof(unsigned char), 1024, fo);
 
 	fwrite(buff, sizeof(unsigned char), (height * width), fo);
 
 	fclose(fo);
 	fclose(streamIn);
+
+
+
 
 	return 0;
 
